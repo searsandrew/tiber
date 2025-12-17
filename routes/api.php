743df5e7ff081abe852ab572418e1552d813a654
Andrew Sears <andrew@mayfifteenth.com>
2025-12-17 12:18:38 -0600

    Add routes and setup User for tokens

--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,9 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/games', [GameController::class, 'store']);
+    Route::get('/games/{game}', [GameController::class, 'show']);
+    Route::post('/games/{game}/actions', [GameActionController::class, 'store']);
+});
