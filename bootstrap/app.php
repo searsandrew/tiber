0ebb9b279188cd8f5327c7fe2af5ad5941b39f95
Andrew Sears <andrew@mayfifteenth.com>
2025-12-30 14:10:12 -0600

    feat: add welcome route to application configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,6 +9,9 @@ return Application::configure(basePath: dirname(__DIR__))
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        then: function () {
+            Route::view('/', 'welcome')->name('welcome');
+        },
     )
     ->withMiddleware(function (Middleware $middleware): void {
         //
